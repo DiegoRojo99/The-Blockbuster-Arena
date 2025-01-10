2de19966b0f1b06e642519eae589f54ecd442226
DiegoRojo99 <diego99rojo@gmail.com>
2025-01-10 21:27:03 +0000

    Added casted

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import Casted from "./components/Casted/Casted";
 
 const App: React.FC = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -25,18 +26,20 @@ const App: React.FC = () => {
         </header>
         <div className={`sidebar ${sidebarOpen ? "open" : ""}`}>
           <nav className="app-nav">
-            {/* <Link to="/game1" className="nav-link" onClick={closeSidebar}>Blurred Poster Game</Link>
-            <Link to="/game2" className="nav-link" onClick={closeSidebar}>Guess the Movie by Cast</Link>
-            <Link to="/game3" className="nav-link" onClick={closeSidebar}>Degrees of Separation</Link>
-            <Link to="/game4" className="nav-link" onClick={closeSidebar}>3x3 Grid Game</Link> */}
+            <Link to="/casted" className="nav-link" onClick={closeSidebar}>Casted</Link>
+            {/* <Link to="/game1" className="nav-link" onClick={closeSidebar}>Blurred Poster Game</Link> */}
+            {/* <Link to="/game2" className="nav-link" onClick={closeSidebar}>Guess the Movie by Cast</Link> */}
+            {/* <Link to="/game3" className="nav-link" onClick={closeSidebar}>Degrees of Separation</Link> */}
+            {/* <Link to="/game4" className="nav-link" onClick={closeSidebar}>3x3 Grid Game</Link> */}
           </nav>
         </div>
         <main className="app-main">
           <Routes>
-            {/* <Route path="/game1" element={<div>Blurred Poster Game Component</div>} />
-            <Route path="/game2" element={<div>Guess the Movie by Cast Component</div>} />
-            <Route path="/game3" element={<div>Degrees of Separation Component</div>} />
-            <Route path="/game4" element={<div>3x3 Grid Game Component</div>} /> */}
+            {/* <Route path="/game1" element={<div>Blurred Poster Game Component</div>} /> */}
+            {/* <Route path="/game2" element={<div>Guess the Movie by Cast Component</div>} /> */}
+            {/* <Route path="/game3" element={<div>Degrees of Separation Component</div>} /> */}
+            {/* <Route path="/game4" element={<div>3x3 Grid Game Component</div>} /> */}
+            <Route path="/casted" element={<Casted />} />
             <Route path="/" element={<div>Welcome to The Blockbuster Arena!</div>} />
           </Routes>
         </main>

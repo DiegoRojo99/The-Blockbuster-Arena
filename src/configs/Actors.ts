483688eb0111e2db6abbb6aa4077d0e5f67cab5a
DiegoRojo99 <diego99rojo@gmail.com>
2025-01-11 00:38:08 +0000

    Actors config

---
+++ src/configs/Actors.ts
@@ -0,0 +1,1042 @@
+let popularActors: any[] = [
+  {
+  "id": 115440,
+  "name": "Sydney Sweeney"
+  },
+  {
+  "id": 1763709,
+  "name": "Aaron Pierre"
+  },
+  {
+  "id": 500,
+  "name": "Tom Cruise"
+  },
+  {
+  "id": 6384,
+  "name": "Keanu Reeves"
+  },
+  {
+  "id": 976,
+  "name": "Jason Statham"
+  },
+  {
+  "id": 1190668,
+  "name": "Timothée Chalamet"
+  },
+  {
+  "id": 974169,
+  "name": "Jenna Ortega"
+  },
+  {
+  "id": 64,
+  "name": "Gary Oldman"
+  },
+  {
+  "id": 131,
+  "name": "Jake Gyllenhaal"
+  },
+  {
+  "id": 224513,
+  "name": "Ana de Armas"
+  },
+  {
+  "id": 1392137,
+  "name": "Margaret Qualley"
+  },
+  {
+  "id": 109513,
+  "name": "Alexandra Daddario"
+  },
+  {
+  "id": 18897,
+  "name": "Jackie Chan"
+  },
+  {
+  "id": 2524,
+  "name": "Tom Hardy"
+  },
+  {
+  "id": 1245,
+  "name": "Scarlett Johansson"
+  },
+  {
+  "id": 1253360,
+  "name": "Pedro Pascal"
+  },
+  {
+  "id": 1373737,
+  "name": "Florence Pugh"
+  },
+  {
+  "id": 6193,
+  "name": "Leonardo DiCaprio"
+  },
+  {
+  "id": 2227,
+  "name": "Nicole Kidman"
+  },
+  {
+  "id": 234352,
+  "name": "Margot Robbie"
+  },
+  {
+  "id": 117642,
+  "name": "Jason Momoa"
+  },
+  {
+  "id": 30614,
+  "name": "Ryan Gosling"
+  },
+  {
+  "id": 31,
+  "name": "Tom Hanks"
+  },
+  {
+  "id": 73968,
+  "name": "Henry Cavill"
+  },
+  {
+  "id": 10859,
+  "name": "Ryan Reynolds"
+  },
+  {
+  "id": 18918,
+  "name": "Dwayne Johnson"
+  },
+  {
+  "id": 2963,
+  "name": "Nicolas Cage"
+  },
+  {
+  "id": 18999,
+  "name": "J.K. Simmons"
+  },
+  {
+  "id": 16828,
+  "name": "Chris Evans"
+  },
+  {
+  "id": 137905,
+  "name": "Bill Skarsgård"
+  },
+  {
+  "id": 16483,
+  "name": "Sylvester Stallone"
+  },
+  {
+  "id": 37632,
+  "name": "Eddie Redmayne"
+  },
+  {
+  "id": 18973,
+  "name": "Mila Kunis"
+  },
+  {
+  "id": 1136406,
+  "name": "Tom Holland"
+  },
+  {
+  "id": 505710,
+  "name": "Zendaya"
+  },
+  {
+  "id": 5469,
+  "name": "Ralph Fiennes"
+  },
+  {
+  "id": 1813,
+  "name": "Anne Hathaway"
+  },
+  {
+  "id": 71070,
+  "name": "Amanda Seyfried"
+  },
+  {
+  "id": 933238,
+  "name": "Rebecca Ferguson"
+  },
+  {
+  "id": 1172108,
+  "name": "Mckenna Grace"
+  },
+  {
+  "id": 3416,
+  "name": "Demi Moore"
+  },
+  {
+  "id": 8691,
+  "name": "Zoe Saldaña"
+  },
+  {
+  "id": 85,
+  "name": "Johnny Depp"
+  },
+  {
+  "id": 192,
+  "name": "Morgan Freeman"
+  },
+  {
+  "id": 119592,
+  "name": "Aubrey Plaza"
+  },
+  {
+  "id": 17605,
+  "name": "Idris Elba"
+  },
+  {
+  "id": 287,
+  "name": "Brad Pitt"
+  },
+  {
+  "id": 38673,
+  "name": "Channing Tatum"
+  },
+  {
+  "id": 206,
+  "name": "Jim Carrey"
+  },
+  {
+  "id": 501,
+  "name": "Dakota Fanning"
+  },
+  {
+  "id": 3223,
+  "name": "Robert Downey Jr."
+  },
+  {
+  "id": 880,
+  "name": "Ben Affleck"
+  },
+  {
+  "id": 11288,
+  "name": "Robert Pattinson"
+  },
+  {
+  "id": 5293,
+  "name": "Willem Dafoe"
+  },
+  {
+  "id": 4724,
+  "name": "Kevin Bacon"
+  },
+  {
+  "id": 1892,
+  "name": "Matt Damon"
+  },
+  {
+  "id": 3967,
+  "name": "Kate Beckinsale"
+  },
+  {
+  "id": 56734,
+  "name": "Chloë Grace Moretz"
+  },
+  {
+  "id": 90633,
+  "name": "Gal Gadot"
+  },
+  {
+  "id": 4587,
+  "name": "Halle Berry"
+  },
+  {
+  "id": 6885,
+  "name": "Charlize Theron"
+  },
+  {
+  "id": 1038,
+  "name": "Jodie Foster"
+  },
+  {
+  "id": 1158,
+  "name": "Al Pacino"
+  },
+  {
+  "id": 2034418,
+  "name": "Jacob Elordi"
+  },
+  {
+  "id": 74568,
+  "name": "Chris Hemsworth"
+  },
+  {
+  "id": 18277,
+  "name": "Sandra Bullock"
+  },
+  {
+  "id": 2037,
+  "name": "Cillian Murphy"
+  },
+  {
+  "id": 2025252,
+  "name": "Ester Expósito"
+  },
+  {
+  "id": 69899,
+  "name": "Zachary Levi"
+  },
+  {
+  "id": 72129,
+  "name": "Jennifer Lawrence"
+  },
+  {
+  "id": 587020,
+  "name": "Theo James"
+  },
+  {
+  "id": 139,
+  "name": "Uma Thurman"
+  },
+  {
+  "id": 37625,
+  "name": "Andrew Garfield"
+  },
+  {
+  "id": 13240,
+  "name": "Mark Wahlberg"
+  },
+  {
+  "id": 19537,
+  "name": "Megan Fox"
+  },
+  {
+  "id": 83271,
+  "name": "Glen Powell"
+  },
+  {
+  "id": 4095689,
+  "name": "Angelica Hart"
+  },
+  {
+  "id": 125025,
+  "name": "Joey King"
+  },
+  {
+  "id": 2203,
+  "name": "Neal McDonough"
+  },
+  {
+  "id": 227454,
+  "name": "Alicia Vikander"
+  },
+  {
+  "id": 86654,
+  "name": "Austin Butler"
+  },
+  {
+  "id": 934289,
+  "name": "Kiernan Shipka"
+  },
+  {
+  "id": 118545,
+  "name": "Dakota Johnson"
+  },
+  {
+  "id": 71580,
+  "name": "Benedict Cumberbatch"
+  },
+  {
+  "id": 10297,
+  "name": "Matthew McConaughey"
+  },
+  {
+  "id": 10912,
+  "name": "Eva Green"
+  },
+  {
+  "id": 4690,
+  "name": "Christopher Walken"
+  },
+  {
+  "id": 17244,
+  "name": "Hayden Christensen"
+  },
+  {
+  "id": 1223786,
+  "name": "Emilia Clarke"
+  },
+  {
+  "id": 3,
+  "name": "Harrison Ford"
+  },
+  {
+  "id": 77700,
+  "name": "Travis Fimmel"
+  },
+  {
+  "id": 222121,
+  "name": "Ben Schwartz"
+  },
+  {
+  "id": 11702,
+  "name": "Adam Brody"
+  },
+  {
+  "id": 17288,
+  "name": "Michael Fassbender"
+  },
+  {
+  "id": 934,
+  "name": "Russell Crowe"
+  },
+  {
+  "id": 226001,
+  "name": "Ariana Grande"
+  },
+  {
+  "id": 15277,
+  "name": "Jon Favreau"
+  },
+  {
+  "id": 2497156,
+  "name": "Elizabeth Roberts"
+  },
+  {
+  "id": 1397778,
+  "name": "Anya Taylor-Joy"
+  },
+  {
+  "id": 65717,
+  "name": "Jon Hamm"
+  },
+  {
+  "id": 524,
+  "name": "Natalie Portman"
+  },
+  {
+  "id": 27319,
+  "name": "Christoph Waltz"
+  },
+  {
+  "id": 62861,
+  "name": "Andy Samberg"
+  },
+  {
+  "id": 112,
+  "name": "Cate Blanchett"
+  },
+  {
+  "id": 1767250,
+  "name": "Ariana Greenblatt"
+  },
+  {
+  "id": 10882,
+  "name": "Rosamund Pike"
+  },
+  {
+  "id": 55638,
+  "name": "Kevin Hart"
+  },
+  {
+  "id": 22226,
+  "name": "Paul Rudd"
+  },
+  {
+  "id": 1765068,
+  "name": "Cynthia Erivo"
+  },
+  {
+  "id": 19034,
+  "name": "Evangeline Lilly"
+  },
+  {
+  "id": 60073,
+  "name": "Brie Larson"
+  },
+  {
+  "id": 62,
+  "name": "Bruce Willis"
+  },
+  {
+  "id": 15111,
+  "name": "Jean-Claude Van Damme"
+  },
+  {
+  "id": 1269,
+  "name": "Kevin Costner"
+  },
+  {
+  "id": 103,
+  "name": "Mark Ruffalo"
+  },
+  {
+  "id": 54882,
+  "name": "Morena Baccarin"
+  },
+  {
+  "id": 91606,
+  "name": "Tom Hiddleston"
+  },
+  {
+    "id": 2282,
+    "name": "Ben Kingsley"
+  },
+  {
+    "id": 28782,
+    "name": "Monica Bellucci"
+  },
+  {
+    "id": 1231,
+    "name": "Julianne Moore"
+  },
+  {
+    "id": 776,
+    "name": "Eddie Murphy"
+  },
+  {
+    "id": 10825,
+    "name": "Donal Logue"
+  },
+  {
+    "id": 140,
+    "name": "Lucy Liu"
+  },
+  {
+    "id": 5081,
+    "name": "Emily Blunt"
+  },
+  {
+    "id": 19292,
+    "name": "Adam Sandler"
+  },
+  {
+    "id": 10205,
+    "name": "Sigourney Weaver"
+  },
+  {
+    "id": 6065,
+    "name": "Dennis Quaid"
+  },
+  {
+    "id": 9281,
+    "name": "Elizabeth Banks"
+  },
+    {
+        "id": 52605,
+        "name": "Eva Longoria"
+    },
+    {
+        "id": 204,
+        "name": "Kate Winslet"
+    },
+    {
+        "id": 53714,
+        "name": "Rachel McAdams"
+    },
+    {
+        "id": 54693,
+        "name": "Emma Stone"
+    },
+    {
+        "id": 19278,
+        "name": "Bill Hader"
+    },
+    {
+        "id": 5292,
+        "name": "Denzel Washington"
+    },
+    {
+        "id": 2283,
+        "name": "Stanley Tucci"
+    },
+    {
+        "id": 4491,
+        "name": "Jennifer Aniston"
+    },
+    {
+        "id": 29222,
+        "name": "Zac Efron"
+    },
+    {
+        "id": 41229,
+        "name": "Meg Foster"
+    },
+    {
+        "id": 1303037,
+        "name": "Taron Egerton"
+    },
+      {
+          "id": 81350,
+          "name": "Toshiaki Karasawa"
+      },
+      {
+          "id": 65510,
+          "name": "Mayumi Tanaka"
+      },
+      {
+          "id": 25147,
+          "name": "Tony Hale"
+      },
+      {
+          "id": 3775928,
+          "name": "Julia Victoria"
+      },
+      {
+          "id": 18050,
+          "name": "Elle Fanning"
+      },
+      {
+          "id": 134,
+          "name": "Jamie Foxx"
+      },
+      {
+          "id": 3292,
+          "name": "Nicholas Hoult"
+      },
+      {
+          "id": 1543364,
+          "name": "Diane Morgan"
+      },
+      {
+          "id": 51797,
+          "name": "Nathan Fillion"
+      },
+      {
+          "id": 39995,
+          "name": "Michael Cera"
+      },
+      {
+          "id": 4785,
+          "name": "Jeff Goldblum"
+      },
+      {
+          "id": 116,
+          "name": "Keira Knightley"
+      },
+      {
+          "id": 3490,
+          "name": "Adrien Brody"
+      },
+      {
+          "id": 2231,
+          "name": "Samuel L. Jackson"
+      },
+      {
+          "id": 58225,
+          "name": "Zach Galifianakis"
+      },
+      {
+          "id": 1204320,
+          "name": "Taiki Nakabayashi"
+      },
+      {
+          "id": 1331457,
+          "name": "Sofia Carson"
+      },
+      {
+          "id": 69597,
+          "name": "Drew Barrymore"
+      },
+      {
+          "id": 26457,
+          "name": "Matthew Lillard"
+      },
+      {
+          "id": 368,
+          "name": "Reese Witherspoon"
+      },
+    {
+        "id": 819,
+        "name": "Edward Norton"
+    },
+    {
+        "id": 42802,
+        "name": "Salman Khan"
+    },
+    {
+        "id": 26472,
+        "name": "Steve Guttenberg"
+    },
+    {
+        "id": 3896,
+        "name": "Liam Neeson"
+    },
+    {
+        "id": 1997928,
+        "name": "Jin Joo"
+    },
+    {
+        "id": 6968,
+        "name": "Hugh Jackman"
+    },
+    {
+        "id": 7906,
+        "name": "Jennifer Tilly"
+    },
+    {
+        "id": 73457,
+        "name": "Chris Pratt"
+    },
+    {
+        "id": 9273,
+        "name": "Amy Adams"
+    },
+    {
+        "id": 1333,
+        "name": "Andy Serkis"
+    },
+    {
+        "id": 1289968,
+        "name": "Cara Delevingne"
+    },
+    {
+        "id": 17838,
+        "name": "Rami Malek"
+    },
+    {
+        "id": 45999,
+        "name": "Takao Osawa"
+    },
+    {
+        "id": 8891,
+        "name": "John Travolta"
+    },
+    {
+        "id": 21200,
+        "name": "Will Arnett"
+    },
+    {
+        "id": 21007,
+        "name": "Jonah Hill"
+    },
+    {
+        "id": 3124,
+        "name": "Valeria Golino"
+    },
+    {
+        "id": 1438264,
+        "name": "Leo Wu"
+    },
+    {
+        "id": 550843,
+        "name": "Elizabeth Olsen"
+    },
+    {
+        "id": 1883366,
+        "name": "Yoo Ji-won"
+    },
+    {
+        "id": 2201315,
+        "name": "Emma Mackey"
+    },
+    {
+        "id": 52139,
+        "name": "Seth MacFarlane"
+    },
+    {
+        "id": 70851,
+        "name": "Jack Black"
+    },
+    {
+        "id": 2275144,
+        "name": "Stephanie Nur"
+    },
+    {
+        "id": 90135,
+        "name": "Marina Inoue"
+    },
+    {
+        "id": 2314,
+        "name": "Peter Falk"
+    },
+    {
+        "id": 20089,
+        "name": "Jena Malone"
+    },
+    {
+        "id": 81243,
+        "name": "Romi Park"
+    },
+    {
+        "id": 23524,
+        "name": "Veronica Ferres"
+    },
+    {
+        "id": 26291,
+        "name": "Martin Henderson"
+    },
+    {
+        "id": 25702,
+        "name": "Rhona Mitra"
+    },
+    {
+        "id": 10980,
+        "name": "Daniel Radcliffe"
+    },
+    {
+        "id": 112205,
+        "name": "Satsuki Yukino"
+    },
+    {
+        "id": 2232,
+        "name": "Michael Keaton"
+    },
+    {
+        "id": 25072,
+        "name": "Oscar Isaac"
+    },
+    {
+        "id": 2590519,
+        "name": "Han Yi-seul"
+    },
+    {
+        "id": 58608,
+        "name": "Shinya Tsukamoto"
+    },
+    {
+        "id": 15110,
+        "name": "Brooke Shields"
+    },
+    {
+        "id": 74022,
+        "name": "Gao Yuanyuan"
+    },
+    {
+        "id": 12052,
+        "name": "Gwyneth Paltrow"
+    },
+    {
+        "id": 1135177,
+        "name": "Loletta Lee Lai-Chun"
+    },
+    {
+        "id": 3489,
+        "name": "Naomi Watts"
+    },
+    {
+        "id": 1642789,
+        "name": "Madelyn Cline"
+    },
+    {
+        "id": 21684,
+        "name": "Bong Joon-ho"
+    },
+    {
+        "id": 1428070,
+        "name": "Isabela Merced"
+    },
+    {
+        "id": 115150,
+        "name": "Kaya Scodelario"
+    },
+    {
+        "id": 4090,
+        "name": "Shirley MacLaine"
+    },
+    {
+        "id": 138,
+        "name": "Quentin Tarantino"
+    },
+    {
+        "id": 21088,
+        "name": "Alan Tudyk"
+    },
+    {
+        "id": 3032312,
+        "name": "Noah Beck"
+    },
+    {
+        "id": 85825,
+        "name": "Leighton Meester"
+    },
+    {
+        "id": 1168909,
+        "name": "Nao Toyama"
+    },
+    {
+        "id": 1878952,
+        "name": "Song Kang"
+    },
+    {
+        "id": 17419,
+        "name": "Bryan Cranston"
+    },
+    {
+        "id": 218144,
+        "name": "Rei Sakuma"
+    },
+    {
+        "id": 11150,
+        "name": "Gina Gershon"
+    },
+    {
+        "id": 1042728,
+        "name": "Úrsula Corberó"
+    },
+    {
+        "id": 517,
+        "name": "Pierce Brosnan"
+    },
+    {
+        "id": 52404,
+        "name": "Mae Whitman"
+    },
+    {
+        "id": 992869,
+        "name": "Ayane Sakura"
+    },
+    {
+        "id": 20750,
+        "name": "Judy Greer"
+    },
+    {
+        "id": 53256,
+        "name": "Terry Crews"
+    },
+    {
+        "id": 2604515,
+        "name": "Emma Myers"
+    },
+    {
+        "id": 23880,
+        "name": "Steven Seagal"
+    },
+    {
+        "id": 151182,
+        "name": "Misaki Kuno"
+    },
+    {
+        "id": 1254672,
+        "name": "Asami Seto"
+    },
+    {
+        "id": 80860,
+        "name": "Jonathan Bailey"
+    },
+    {
+        "id": 1171570,
+        "name": "Jacob Lofland"
+    },
+    {
+        "id": 56365,
+        "name": "Charlie Hunnam"
+    },
+    {
+        "id": 129700,
+        "name": "Nozomi Sasaki"
+    },
+    {
+        "id": 1590797,
+        "name": "Sadie Sink"
+    },
+    {
+        "id": 41421,
+        "name": "Jennifer Morrison"
+    },
+    {
+        "id": 23532,
+        "name": "Jason Bateman"
+    },
+    {
+        "id": 1260846,
+        "name": "Yeung Si-min"
+    },
+    {
+        "id": 3293,
+        "name": "Rachel Weisz"
+    },
+    {
+        "id": 13548,
+        "name": "James Spader"
+    },
+    {
+        "id": 17064,
+        "name": "Ben Whishaw"
+    },
+    {
+        "id": 31167,
+        "name": "Elizabeth Mitchell"
+    },
+    {
+        "id": 1468490,
+        "name": "Kang Eun-hye"
+    },
+    {
+        "id": 1328,
+        "name": "Sean Astin"
+    },
+    {
+        "id": 4173,
+        "name": "Anthony Hopkins"
+    },
+    {
+        "id": 62831,
+        "name": "Will Forte"
+    },
+    {
+        "id": 3291094,
+        "name": "Budsarin Wonglelanont"
+    },
+    {
+        "id": 129104,
+        "name": "Melissa Benoist"
+    },
+    {
+        "id": 118178,
+        "name": "Virginie Efira"
+    },
+    {
+        "id": 572020,
+        "name": "Anne Watanabe"
+    },
+    {
+        "id": 115305,
+        "name": "Ryusei Nakao"
+    },
+    {
+        "id": 9195,
+        "name": "Hiroyuki Sanada"
+    },
+    {
+        "id": 1459885,
+        "name": "Lily-Rose Depp"
+    },
+    {
+        "id": 2324569,
+        "name": "Emma Corrin"
+    },
+    {
+        "id": 1253353,
+        "name": "Noah Centineo"
+    },
+    {
+        "id": 41091,
+        "name": "Kristen Wiig"
+    },
+    {
+        "id": 23626,
+        "name": "Liev Schreiber"
+    },
+    {
+        "id": 130640,
+        "name": "Hailee Steinfeld"
+    },
+    {
+        "id": 1281,
+        "name": "Freddie Highmore"
+    },
+    {
+        "id": 17276,
+        "name": "Gerard Butler"
+    },
+    {
+        "id": 6614,
+        "name": "Cole Hauser"
+    },
+    {
+        "id": 77927,
+        "name": "Megumi Ogata"
+    },
+    {
+        "id": 203268,
+        "name": "Yukari Tamura"
+    },
+    {
+        "id": 1327,
+        "name": "Ian McKellen"
+    },
+];
+  
+export {
+  popularActors
+};

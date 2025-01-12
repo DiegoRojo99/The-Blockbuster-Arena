72cef2862ec96d2bd720721e02edeeae23845b60
DiegoRojo99 <diego99rojo@gmail.com>
2025-01-12 18:41:23 +0000

    Actual grid values

--- src/components/Grid/predefinedGames.ts
+++ src/components/Grid/predefinedGames.ts
@@ -1,12 +1,26 @@
-export const predefinedGames: string[][][] = [
-  [
-    ["Movie A", "Movie B", "Movie C"],
-    ["Movie D", "Movie E", "Movie F"],
-    ["Movie G", "Movie H", "Movie I"],
-  ],
-  [
-    ["Movie J", "Movie K", "Movie L"],
-    ["Movie M", "Movie N", "Movie O"],
-    ["Movie P", "Movie Q", "Movie R"],
-  ],
+export const predefinedGames = [
+  {
+    rowMovies: [
+      { title: "Inception", poster: "https://image.tmdb.org/t/p/w200/8sNtGQmABJ75gr16vM3deMEUcwr.jpg" },
+      { title: "Titanic", poster: "https://image.tmdb.org/t/p/w200/kHXEpyfl6zqn8a6YuozZUujufXf.jpg" },
+      { title: "Pulp Fiction", poster: "https://image.tmdb.org/t/p/w200/dM2w364MScsjFf8pfMbaWUcWrR.jpg" },
+    ],
+    columnMovies: [
+      { title: "The Dark Knight", poster: "https://image.tmdb.org/t/p/w200/qJ2tW6WMUDux911r6m7haRef0WH.jpg" },
+      { title: "Shutter Island", poster: "https://image.tmdb.org/t/p/w200/52mC5D2f5Xf71IN97D63Lfp2huq.jpg" },
+      { title: "Kill Bill", poster: "https://image.tmdb.org/t/p/w200/4jVPI74b4U7UWzo4taMj3tAYFfU.jpg" },
+    ],
+  },
+  {
+    rowMovies: [
+      { title: "Iron Man", poster: "https://image.tmdb.org/t/p/w200/78lPtwv72eTNqFW9COBYI0dWDJa.jpg" },
+      { title: "Doctor Strange", poster: "https://image.tmdb.org/t/p/w200/uGBVj3bEbCoZbDjjl9wTxcygko1.jpg" },
+      { title: "Black Panther", poster: "https://image.tmdb.org/t/p/w200/uxzzxijgPIY7slzFvMotPv8wjKA.jpg" },
+    ],
+    columnMovies: [
+      { title: "Avengers: Endgame", poster: "https://image.tmdb.org/t/p/w200/or06FN3Dka5tukK1e9sl16pB3iy.jpg" },
+      { title: "Guardians of the Galaxy", poster: "https://image.tmdb.org/t/p/w200/r7vmZjiyZw9rpJMQJdXpjgiCOk9.jpg" },
+      { title: "Captain Marvel", poster: "https://image.tmdb.org/t/p/w200/AtsgWhDnHTq68L0lLsUrCnM7TjG.jpg" },
+    ],
+  },
 ];

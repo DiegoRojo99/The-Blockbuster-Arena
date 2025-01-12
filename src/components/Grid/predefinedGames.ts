205e5a3bc58b54de02715bba3f60910e910c098e
DiegoRojo99 <diego99rojo@gmail.com>
2025-01-12 18:01:00 +0000

    Add basic predefined game

---
+++ src/components/Grid/predefinedGames.ts
@@ -0,0 +1,12 @@
+export const predefinedGames: string[][][] = [
+  [
+    ["Movie A", "Movie B", "Movie C"],
+    ["Movie D", "Movie E", "Movie F"],
+    ["Movie G", "Movie H", "Movie I"],
+  ],
+  [
+    ["Movie J", "Movie K", "Movie L"],
+    ["Movie M", "Movie N", "Movie O"],
+    ["Movie P", "Movie Q", "Movie R"],
+  ],
+];
